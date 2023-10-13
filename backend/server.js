de2cb50e46314a1a3f42ba767c6d9c591cb7fb04
JaGarcia02 <jagarcia02051998@gmail.com>
2023-10-13 14:53:16 +0800

    database set up done using mongodb and .env has been setup, all is working and project is now connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
 import expres from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 const port = process.env.PORT || 3000;
 const app = expres();
 
-app.listen(port, () => {
-  console.log(`Server running on ${port}`);
-});
+mongoose
+  .connect(process.env.MONGO_DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(port, () => {
+      console.log("Database: Connected");
+      console.log(`Server Port: ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
